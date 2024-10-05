92677f358fe5afcb680705dc9e5a5c8ea7307ae5
edoski <87473803+edoski@users.noreply.github.com>
2024-10-05 18:15:18 +0200

    Setting up a threaded basic client-server messaging modality for users in shared space/general chat

--- Client.java
+++ Client.java
@@ -1,2 +1,55 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+	private Socket socket;
+	private BufferedReader in;
+	private BufferedWriter out;
+	private String username;
+
+	public Client(Socket socket, String username) {
+		try {
+			this.username = username;
+			this.socket = socket;
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		} catch (IOException e) {
+			closeEverything(socket, in, out);
+		}
+	}
+
+	public void sendMessage() {
+		try {
+			out.write(username);
+			out.newLine();
+			out.flush();
+
+			Scanner scanner = new Scanner(System.in);
+			while (socket.isConnected()) {
+				String message = scanner.nextLine();
+				out.write(username + ": " + message);
+				out.newLine();
+				out.flush();
+			}
+		} catch (IOException e) {
+			closeEverything(socket, in, out);
+		}
+	}
+
+	public void closeEverything(Socket socket, BufferedReader in, BufferedWriter out) {
+		try {
+			if (socket != null) {
+				socket.close();
+			}
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
